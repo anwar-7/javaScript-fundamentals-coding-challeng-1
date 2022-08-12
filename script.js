9ace3b95dd4e85badc6bc806e6f7ea83d18e6356
anwarul1 <anwarjewel007@gmail.com>
2022-08-12 14:21:56 +0600

    challenge 2 end

--- script.js
+++ script.js
@@ -54,3 +54,33 @@ BMI (28.3) is higher than John's (23.9)!"
 Hint: Use an if/else statement 😉
 GOOD LUCK 😀 
 */
+
+// data 1
+
+// const markMass = 78;
+// const markHeight = 1.69;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// data 2
+
+const markMass = 95;
+const markHeight = 1.88;
+
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+// console.log(markBMI, johnBMI);
+const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+
+if (markHigherBMI) {
+  console.log(`Mark's
+  BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+} else {
+  console.log(`John's (${johnBMI}) is higher than Mark's
+    BMI (${markBMI})!`);
+}

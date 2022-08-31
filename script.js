5b886bea9245fc2d3398c5fc9060e01c30031f80
anwarul1 <anwarjewel007@gmail.com>
2022-08-31 15:58:12 +0600

    DOM and Events coding challenge 1

--- script.js
+++ script.js
@@ -459,3 +459,36 @@ const printForecast = function (arr) {
 const tempsForecast = printForecast([17, 21, 23]);
 console.log(tempsForecast);
  */
+
+// <------------------------------------------------------>
+// <---------- JavaScript in the Browser: DOM and Events ------->
+// <----------------------------------------------------->
+/*
+/* 
+Coding Challenge #1
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK 😀
+*/
+/* 
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+ */
